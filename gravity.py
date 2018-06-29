Made plots prettier and rectified the incorrect PE formula

gravity.py
@@ -8,7 +8,7 @@ G = 1.0
 M = 1.0
 Re = 1.0
 
-def plot_earth(r=Re):
+def plot_earth(fig,r=Re):
 	"""
 	Plots a circle (planet) with the radius given. Default value
 	is the radius of the Earth
@@ -16,8 +16,8 @@ def plot_earth(r=Re):
 	x = np.linspace(-1,1,1000)
 	yp = np.sqrt(r**2 - x**2)
 	yn = -1*np.sqrt(r**2 - x**2)
-	plt.plot(x,yp,color="black")
-	plt.plot(x,yn,color="black")
+	fig.plot(x,yp,color="black")
+	fig.plot(x,yn,color="black")
 
 def derive(params):
 	"""
@@ -45,7 +45,7 @@ def check_in(x,y,R=Re):
 
 
 
-def run(initial_param,N=15000,h=0.001,energy_debug=False):
+def run(initial_param,fig,N=15000,h=0.001,energy_debug=False):
 	"""
 	a function that runs the simulation given the inital parameters, the number
 	of iterations each simulation must go through (default => 15000), and the
@@ -71,12 +71,12 @@ def run(initial_param,N=15000,h=0.001,energy_debug=False):
 		if check_in(data[t+1][0],data[t+1][1]):
 			break
 
-	plt.plot(data[:,0],data[:,1])
+	fig.plot(data[:,0],data[:,1])
 
 	if energy_debug:
 		return data
 
-def plot_energy(data,t):
+def plot_energy(data,t,fig):
 	# data => [x,y,vx,vy]
 	# KE = T = 0.5*m*(vx**2 + vy**2)
 	# PE = U = G*M*m/(x**2 + y**2)**0.5
@@ -84,15 +84,14 @@ def plot_energy(data,t):
 	m = 0.2
 
 	T = 0.5*m*(data[:,2]**2 + data[:,3]**2)
-	U = G*M*m/(np.sqrt(data[:,0]**2 + data[:,1]**2))
+	U = -G*M*m/(np.sqrt(data[:,0]**2 + data[:,1]**2))
 
-	plt.figure()
-	plt.plot(T,label="Kinetic Energy")
-	plt.plot(U,label="Potential Energy")
-	plt.plot((T+U),label="Total Energy")
-	plt.legend()
-	plt.savefig("energy-plot-{}.pdf".format(t))
-	plt.close()
+
+	fig.plot(T,label="Kinetic Energy")
+	fig.plot(U,label="Potential Energy")
+	fig.plot((T+U),label="Total Energy")
+	fig.legend()
+
 
 
 
@@ -101,16 +100,23 @@ print("simulation start")
 
 for (t,vxs) in enumerate(np.linspace(1,1.3,5)):
 	# running through all vxs from 1 to 1.3
-	plt.figure()
-	plt.axis('equal')
-	plot_earth()
-	data = run([0,1.0,vxs,0],energy_debug=True)
-	plt.savefig("orbit-vx-{}.pdf".format(t))
+	
+	# creating multiple plots for the same orbit
+
+	fig, axs = plt.subplots(1,2,figsize=(19,10))
+
+	plot_earth(fig=axs[0])
+	axs[0].axis('equal')
+	data = run([0,1.0,vxs,0],energy_debug=True,fig=axs[0])
+
+	plot_energy(data,t,fig=axs[1])
+	plt.savefig("orbit-energy-vx-{}.pdf".format(t))
 	plt.close()
 
 	# comment out below code if no energy-debug
 
-	plot_energy(data,t)
+
 
 
 print("simulation over.")
+